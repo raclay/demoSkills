53a41e09e9b98691d41edced33ed35a42960b9b9
raclay <97620200+raclay@users.noreply.github.com>
2022-02-03 12:09:04 -0800

    myskillsdemo

---
+++ mySkillsDemo.java
@@ -0,0 +1,5 @@
+public class mySkillsDemo{
+    public static int average(int x, int y){
+        return (x+y)/2;
+    }
+}
---
+++ mySkillsDemoTest.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class mySkillsDemoTest{
+
+    @Test
+    public void averageTest(){
+        assertEquals(2, mySkillsDemo.average(1,1));
+    }
+}
